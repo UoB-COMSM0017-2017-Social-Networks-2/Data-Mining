1e3c81f56015689afc7946be039b749c180f8cba
Vishal Yadav <vishalydv23@gmail.com>
2017-02-23 11:12:41 +0000

    updated tweepy.py
    * Mining tweet based on location and trending topic. 
    * Adding topic and location coordinates to json strings

--- twitter.py
+++ twitter.py
@@ -3,6 +3,7 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 from tweepy import API
+from html.parser import HTMLParser
 import json
 import time
 
@@ -15,13 +16,33 @@ consumer_secret = ""
 
 # sample woeid code for countries USA, UK, Brazil, Canada, India
 #woeidList = ['23424977','23424975','23424768', '23424775', '23424848']
-woeidList = ['23424977']
+woeidList = ['23424975'] #Used to fetch trending topics
+# sample location bounding box coordinates of london
+location = [-0.351468,51.38494,0.148271,51.672343] #Used to fetch all tweets for this location
+TrendingTopics = []
+
 
 #This is a basic listener that just prints received tweets to stdout.
 class StdOutListener(StreamListener):
-
+    #On every tweet arrival
     def on_data(self, data):
-        print (data)
+        #Convert the string data to pyhton json object.
+        data = json.loads(HTMLParser().unescape(data))
+        #Gives the content of the tweet.
+        tweet = data['text']
+        #If tweet content contains any of the trending topic.
+        for topic in TrendingTopics:
+            if topic in tweet: 
+                #Add trending topic and original bounding box as attribute
+                data['TrendingTopic'] = topic
+                data['QueriedBoundingBox'] = location[0]
+                #Convert the json object again to string 
+                dataObj = json.dumps(data)
+                #Appending the data in tweetlondon.json file
+                with open('tweetlondon.json','a') as tf:
+                   tf.write(dataObj)
+                #prints on console
+                print (dataObj)        
         return True
 
     def on_error(self, status):
@@ -41,18 +62,22 @@ if __name__ == '__main__':
     count = 0
     while True:
         count = count + 1
-        #This runs every half an hour
-        print ('Trends Collected for {0} hours'.format(count / 2))
-
+        #This runs every an hour
+        print ('Trends Collected for {0} hours'.format(count))
+        print(count)
+        # for country in location:
         for country in woeidList:
             trends1 = api.trends_place(country)
             data = trends1[0]
             # grab the trends
             trends = data['trends']
             # grab the name from each trend
-            names = [trend['name'] for trend in trends[:10]]
-            
-            stream.filter(track=names)
+            TrendingTopics = [trend['name'] for trend in trends[:10]]
+            # put all the names together with a ' ' separating them
+            #trendsName = ' '.join(names)
+            print(TrendingTopics)
+            #Stream the tweets for given location coordinates
+            stream.filter(locations= [location[0],location[1],location[2],location[3]])
 
-        time.sleep(60*30)
-
+        time.sleep(5)
+        
